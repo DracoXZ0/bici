853d6fda12451beff00983bdfe0b153f5ec33472
Draco <londonosimon67873@gmail.com>
2022-09-30 14:13:19 -0500

    Final

--- src/components/inicioBici.js
+++ src/components/inicioBici.js
@@ -7,16 +7,24 @@ import Mitad from "./mitad";
 function InicioBici() {
 
     const [networks, setNetworks] = useState([])
+    const [networkInfo, setNetworkInfo] = useState ([])
 
     const ConsumirAPI = async () => {
-
         var url = "http://api.citybik.es/v2/networks"
         const response = await fetch(url, { method: 'GET' })
             .then(response => response.json())
             .catch(error => console.log(error))
         setNetworks(response.networks)
+    }
 
-        console.log(networks)
+    const ConsumirAPIdetallada = async (id) => {
+
+        var url = `http://api.citybik.es/v2/networks/${id}`
+        const response = await fetch(url, { method: 'GET' })
+            .then(response => response.json())
+            .catch(error => console.log(error))
+        setNetworkInfo(response.network)
+        
     }
 
     useEffect(() => {
@@ -25,13 +33,13 @@ function InicioBici() {
 
     return (
         <div className="lista_networks">
-            <Mitad></Mitad>
+            <Mitad data={networkInfo}></Mitad>
             {
                 networks.map(network => {
                     return (
                         <div key={network.id} className="box">
                             <Alert variant={'dark'}>
-                                <Alert.Link className="borrar_linea" href="#">{network.name} ({network.location.city})</Alert.Link>
+                                <Alert.Link onClick={()=> ConsumirAPIdetallada(network.id)} className="borrar_linea" href="#">{network.name} ({network.location.city})</Alert.Link>
                             </Alert>
                         </div>
                     )
--- src/components/mitad.js
+++ src/components/mitad.js
@@ -1,7 +1,70 @@
-function Mitad (){
-    return (
-        <div>
+import { useEffect, useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
 
+function Mitad({ data }) {
+    
+    const [total_free_bikes, setAllFB] = useState(null)
+    const [all_empty_slots, setAllES] = useState(null)
+    const estaciones = data.stations
+
+    const handleCount = () => {
+        var temp_estaciones = estaciones
+        var all_empty_slots = 0
+        var all_free_bikes = 0
+        for (var i in temp_estaciones) {
+            all_empty_slots = all_empty_slots + temp_estaciones[i].empty_slots
+            all_free_bikes = all_free_bikes + temp_estaciones[i].free_bikes
+        }
+        setAllFB(all_free_bikes)
+        setAllES(all_empty_slots)
+    }
+
+    useEffect(() => {
+        handleCount()
+    }, [handleCount])
+
+    if (data.length === 0) {
+        return (
+            <div className="contenedor_medio">
+                <Alert variant="success">
+                    <Alert.Heading>Dale Click a una network</Alert.Heading>
+                </Alert>
+            </div>
+        )
+    }
+    return (
+        <div className="contenedor_medio">
+            <Alert className='Alert' variant="success">
+                <Alert.Heading>
+                    {data.name}
+                    <br></br>
+                    Total espacios libres: {all_empty_slots}
+                    <br></br>
+                    Total bicicletas libres: {total_free_bikes}
+                </Alert.Heading>
+                <p className="mb-0">
+                    {data.company} {data.location.country}
+                </p>
+                {
+                    estaciones.map(estacionInterna => {
+                        return (
+                            <div key={estacionInterna.id} className="box">
+                                {estacionInterna.name}
+                                <br></br>
+                                Bicicletas libres: {estacionInterna.free_bikes === null ? "No hay información" : estacionInterna.free_bikes } 
+                                <br></br>
+                                Espacios libres: {estacionInterna.empty_slots === null ? "No hay información" : estacionInterna.empty_slots}
+                                <br></br>
+                                Total espacios: {estacionInterna.empty_slots + estacionInterna.free_bikes}
+                                <br></br>
+                                Ultima actualización: {estacionInterna.timestamp}
+                                <hr></hr>
+                                
+                            </div>
+                        )
+                    })
+                }
+            </Alert>
         </div>
     )
 }
